74054c843070f383150de1bf6294307c30b8cdc4
Keith Avery <slabgorb@gmail.com>
2017-11-07 15:52:35 -0500

    playing around wiht voronoi

--- mapper/map.go
+++ mapper/map.go
@@ -1,6 +1,7 @@
 package mapper
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -11,23 +12,51 @@ import (
 
 var randomizer random.Generator = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 
+type HeightMap struct {
+	diagram voronoi.Diagram
+	heights map[int]float64
+}
+
 func SetRandomizer(g random.Generator) {
 	randomizer = g
 }
 
+func RandomHeight() float64 {
+	return randomizer.Float64()
+}
+
+func RandomCell(diagram voronoi.Diagram) *voronoi.Cell {
+	r := randomizer.Intn(len(diagram.Cells))
+	return diagram.Cells[r]
+}
+
 func VoronoiDiagram(x, y int, pointCount int, relaxation int) *voronoi.Diagram {
+	t := time.Now()
 	sites := []voronoi.Vertex{}
 	for i := 0; i < pointCount; i++ {
 		vx := randomizer.Float64() * float64(x)
 		vy := randomizer.Float64() * float64(y)
 		sites = append(sites, voronoi.Vertex{X: vx, Y: vy})
 	}
+	fmt.Println(time.Since(t))
+	t = time.Now()
 	bbox := voronoi.NewBBox(0, float64(x), 0, float64(y))
 	d := voronoi.ComputeDiagram(sites, bbox, true)
+	fmt.Println(time.Since(t))
 	for i := 0; i < relaxation; i++ {
+		t = time.Now()
 		v := vu.LloydRelaxation(d.Cells)
 		d = voronoi.ComputeDiagram(v, bbox, true)
+		fmt.Println(time.Since(t))
 	}
 	return d
+}
+
+func FindNeighborCells(diagram *voronoi.Diagram, cell *voronoi.Cell) []*voronoi.Cell {
+	cells := []*voronoi.Cell{}
+	for _, h := range cell.Halfedges {
+		cells = append(cells, h.Edge.GetOtherCell(cell))
+	}
+	return cells
 
 }
--- mapper/map_test.go
+++ mapper/map_test.go
@@ -5,17 +5,16 @@ import (
 	"testing"
 
 	"github.com/fogleman/gg"
-	vu "github.com/pzsz/voronoi/utils"
 	. "github.com/slabgorb/gotown/mapper"
 )
 
 func TestDiagram(t *testing.T) {
-	x := 300
-	y := 300
-	diagram := VoronoiDiagram(x, y, 50, 8)
+	x := 600
+	y := 400
+	diagram := VoronoiDiagram(x, y, 8000, 1)
 	patterns := []gg.Pattern{}
-	for i := 1; i < 13; i++ {
-		im, err := gg.LoadPNG(fmt.Sprintf("draw/textures/seamless-watercolor-%d.png", i))
+	for i := 0; i < 12; i++ {
+		im, err := gg.LoadPNG(fmt.Sprintf("draw/textures/%d.png", i))
 		if err != nil {
 			t.Fatal("Could not load pattern")
 		}
@@ -26,17 +25,17 @@ func TestDiagram(t *testing.T) {
 	dc.SetLineWidth(0.5)
 	dc.SetHexColor("#000000")
 	currentPattern := 0
+	dc.SetLineJoinRound()
 	for _, cell := range diagram.Cells {
-		dc.SetLineJoinRound()
-		dc.SetLineWidth(2.0)
+		dc.SetLineWidth(1.0)
 		//		var v *voronoi.EdgeVertex
 		for _, he := range cell.Halfedges {
 			a := he.GetStartpoint()
 			b := he.GetEndpoint()
 			dc.LineTo(a.X, a.Y)
 			dc.LineTo(b.X, b.Y)
-			centroid := vu.CellCentroid(cell)
-			dc.DrawPoint(centroid.X, centroid.Y, 5)
+			//centroid := vu.CellCentroid(cell)
+			//dc.DrawPoint(centroid.X, centroid.Y, 3)
 		}
 		dc.SetFillStyle(patterns[currentPattern])
 		dc.FillPreserve()
@@ -47,12 +46,6 @@ func TestDiagram(t *testing.T) {
 		}
 
 	}
-	// for _, edge := range diagram.Edges {
-	// 	fmt.Printf("%#v", edge)
-	// 	dc.DrawLine(edge.Va.X, edge.Va.Y, edge.Vb.X, edge.Vb.Y)
-	// }
-	// dc.SetFillStyle(patterns[currentPattern])
-	// dc.Fill()
 
 	dc.SavePNG("dg.png")
 }
