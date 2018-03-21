eca33703a6a4f115a89fd4227a189908ae3c1a4c
slabgorb <slabgorb@gmail.com>
2018-03-21 16:56:44 -0400

    nav changes

--- web/src/components/App/Show.jsx
+++ web/src/components/App/Show.jsx
@@ -4,13 +4,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
-import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
 import Typography from 'material-ui/Typography';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import { withRouter } from 'react-router-dom';
 import axios from 'axios';
-import SimpleBottomNavigation from './SimpleBottomNavigation';
+
 
 const ralewayFF = ({ fontFamily: 'Raleway' });
 const montserratFF = ({ fontFamily: 'Montserrat' });
+
+const styles = () => ({
+  flex: { flex: 1 },
+  main: { marginTop: '75px' },
+  appBar: {},
+});
 
 const theme = createMuiTheme({
   palette: {
@@ -45,31 +54,81 @@ const theme = createMuiTheme({
 
 const seed = () => axios.put('/api/seed');
 
-const App = ({ children, classes }) =>
-  (
-    <div>
-      <Reboot>
-        <MuiThemeProvider theme={theme}>
-          <div>
-            <AppBar position="static">
-              <Toolbar>
-                <Typography variant="title" color="inherit" className={classes.flex}>
-                  Gotown
-                </Typography>
-                <Button variant="raised" onClick={() => seed()}>Seed</Button>
-              </Toolbar>
-            </AppBar>
-            {children}
-            <SimpleBottomNavigation />
-          </div>
-        </MuiThemeProvider>
-      </Reboot>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+    };
+    this.handleMenu = this.handleMenu.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleMenu(event) {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleMenuItem(value) {
+    const { history } = this.props;
+    history.push(`/${value}`)
+    this.handleClose();
+  }
+
+  handleClose() {
+    this.setState({ anchorEl: null });
+  }
+
+  render() {
+    const { children, classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+    return (
+      <div>
+        <Reboot>
+          <MuiThemeProvider theme={theme}>
+            <div>
+              <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                  <Typography variant="title" color="inherit" className={classes.flex}>
+                    Gotown
+                  </Typography>
+                  <IconButton onClick={this.handleMenu}>
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-bar"
+                    open={open}
+                    onClose={this.handleClose}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                  >
+                    <MenuItem onClick={() => this.handleMenuItem('species')}>Species</MenuItem>
+                    <MenuItem onClick={() => this.handleMenuItem('cultures')}>Cultures</MenuItem>
+                    <MenuItem onClick={() => this.handleMenuItem('towns')}>Towns</MenuItem>
+                    <MenuItem onClick={() => seed()}>Seed</MenuItem>
+                  </Menu>
+                </Toolbar>
+              </AppBar>
+              <div className={classes.main}>
+                {children}
+              </div>
+            </div>
+          </MuiThemeProvider>
+        </Reboot>
+      </div>
+    );
+  }
+
+}
+
 
 App.propTypes = {
   children: PropTypes.node.isRequired,
   classes: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
-export default withStyles(theme)(App);
+export default withRouter(withStyles(styles)(App));
