56e7f0f1ffdbcaa515b3c072bc4ee8ac03ec6fcb
Keith Avery <slabgorb@gmail.com>
2018-05-11 17:51:48 -0400

    fun heraldry stuff

--- heraldry/heraldry.go
+++ heraldry/heraldry.go
@@ -1,6 +1,7 @@
 package heraldry
 
 import (
+	"image"
 	"image/color"
 
 	"github.com/fogleman/gg"
@@ -30,27 +31,117 @@ var Colors = Tincture{
 	"cendree":  color.RGBA{R: 0x80, G: 0x80, B: 0x80, A: 0xff},
 }
 
-func Apply(dc *gg.Context) error {
-	dc.DrawRectangle(0, 0, float64(dc.Width()), float64(dc.Height()))
-	dc.SetColor(Colors["sable"])
-	dc.Fill()
-	heaterShield(dc, Metals["or"])
-	dc.SavePNG("test.png")
-	return nil
+type Fill func(dc *gg.Context)
+type Shape func(dc *gg.Context) *image.Alpha
+
+// Escutcheon represents the 'shield' part of a heraldric device
+type Escutcheon struct {
+	Shape
+	Fill
 }
 
-func heaterShield(dc *gg.Context, fillColor color.Color) {
+// Render draws the Escutcheon
+func (e Escutcheon) Render(dc *gg.Context) {
+	mask := e.Shape(dc)
+	dc.SetMask(mask)
+	e.Fill(dc)
+}
+
+// PerFess divides the field horizontally
+func PerFess(top color.Color, bottom color.Color) Fill {
+	return func(dc *gg.Context) {
+		h := float64(dc.Height())
+		w := float64(dc.Width())
+		divide := h / 2
+		dc.DrawRectangle(0, 0, w, divide)
+		dc.SetColor(top)
+		dc.Fill()
+		dc.DrawRectangle(0, divide, w, divide)
+		dc.SetColor(bottom)
+		dc.Fill()
+	}
+}
+
+// PerChevron divides the field 'after the manner of a chevron'
+func PerChevron(top color.Color, bottom color.Color) Fill {
+	return func(dc *gg.Context) {
+		h := float64(dc.Height())
+		w := float64(dc.Width())
+		divide := h / 2
+		divideW := w / 2
+		dc.SetColor(top)
+		dc.DrawRectangle(0, 0, w, h)
+		dc.Fill()
+		dc.MoveTo(0, h)
+		dc.LineTo(divideW, divide)
+		dc.LineTo(w, h)
+		dc.LineTo(0, h)
+		dc.SetColor(bottom)
+		dc.Fill()
+	}
+}
+
+// PerBend divides the field diagonally from upper left to lower right
+func PerBend(left color.Color, right color.Color) Fill {
+	return func(dc *gg.Context) {
+		h := float64(dc.Height())
+		w := float64(dc.Width())
+		dc.MoveTo(0, 0)
+		dc.LineTo(0, h)
+		dc.LineTo(w, h)
+		dc.LineTo(0, 0)
+		dc.SetColor(left)
+		dc.Fill()
+		dc.MoveTo(w, 0)
+		dc.LineTo(w, h)
+		dc.LineTo(0, 0)
+		dc.LineTo(w, 0)
+		dc.SetColor(right)
+		dc.Fill()
+	}
+}
+
+// PerBendSinister divides the field diagonally from upper right to lower left
+func PerBendSinister(left color.Color, right color.Color) Fill {
+	fill := PerBend(left, right)
+	return func(dc *gg.Context) {
+		h := float64(dc.Height())
+		w := float64(dc.Width())
+		dc.RotateAbout(3.14159, w/2, h/2)
+		fill(dc)
+	}
+}
+
+func PerPale(left color.Color, right color.Color) Fill {
+	return func(dc *gg.Context) {
+		h := float64(dc.Height())
+		w := float64(dc.Width())
+		divide := w / 2
+		dc.DrawRectangle(0, 0, divide, h)
+		dc.SetColor(left)
+		dc.Fill()
+		dc.DrawRectangle(divide, 0, divide, h)
+		dc.SetColor(right)
+		dc.Fill()
+	}
+}
+
+func Solid(fillColor color.Color) Fill {
+	return func(dc *gg.Context) {
+		dc.SetColor(fillColor)
+		dc.Fill()
+	}
+}
+
+func HeaterShield(dc *gg.Context) *image.Alpha {
 	w := float64(dc.Width())
 	h := float64(dc.Height())
 	margin := float64(w / 10)
 	halfHeater(dc, w, h, margin)
-	dc.SetColor(fillColor)
-	dc.Fill()
-	dc.Push()
 	dc.ScaleAbout(-1, 1, w/2, h/2)
 	halfHeater(dc, w, h, margin)
-	dc.SetColor(fillColor)
 	dc.Fill()
+	return dc.AsMask()
 }
 
 func halfHeater(dc *gg.Context, w, h, margin float64) {
--- heraldry/heraldry_test.go
+++ heraldry/heraldry_test.go
@@ -1,13 +1,74 @@
 package heraldry_test
 
 import (
-	"testing"
-
+	"fmt"
 	"github.com/fogleman/gg"
 	. "github.com/slabgorb/gotown/heraldry"
+	"image"
+	"testing"
 )
 
-func TestApply(t *testing.T) {
+func compareImages(name string) bool {
+	testResult, _ := gg.LoadImage(fmt.Sprintf("%s.png", name))
+	reference, _ := gg.LoadImage(fmt.Sprintf("reference_images/%s.png", name))
+	if testResult.Bounds() != reference.Bounds() {
+		return false
+	}
+	bounds := reference.Bounds()
+	for i := bounds.Min.X; i < bounds.Max.X; i++ {
+		for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
+			if testResult.At(i, j) != reference.At(i, j) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func TestEscutcheon(t *testing.T) {
+	type testCase struct {
+		draw func(dc *gg.Context) *image.Alpha
+		fill func(dc *gg.Context)
+		name string
+	}
 	dc := gg.NewContext(300, 300)
-	Apply(dc)
+	testCases := []testCase{
+		testCase{
+			name: "heater_per_chevron",
+			draw: HeaterShield,
+			fill: PerChevron(Colors["sable"], Metals["or"]),
+		},
+		testCase{
+			name: "heater_per_pale",
+			draw: HeaterShield,
+			fill: PerPale(Colors["sable"], Metals["or"]),
+		},
+		testCase{
+			name: "heater_per_fess",
+			draw: HeaterShield,
+			fill: PerFess(Colors["sable"], Metals["or"]),
+		},
+		testCase{
+			name: "heater_per_bend",
+			draw: HeaterShield,
+			fill: PerBend(Colors["sable"], Metals["or"]),
+		},
+		testCase{
+			name: "heater_per_bend_sinister",
+			draw: HeaterShield,
+			fill: PerBendSinister(Colors["sable"], Metals["or"]),
+		},
+	}
+	for _, tc := range testCases {
+		e := Escutcheon{
+			Shape: tc.draw,
+			Fill:  tc.fill,
+		}
+		e.Render(dc)
+		dc.SavePNG(fmt.Sprintf("%s.png", tc.name))
+		if !compareImages(tc.name) {
+			t.Errorf("images are not equal for %s", tc.name)
+		}
+	}
+
 }
