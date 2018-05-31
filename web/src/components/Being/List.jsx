8c3925cdaed78c5254019de493e4cff76a6e7834
Keith Avery <slabgorb@gmail.com>
2018-05-31 10:30:28 -0400

    table formatting

--- web/src/components/Being/List.jsx
+++ web/src/components/Being/List.jsx
@@ -1,39 +1,74 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 import Table from 'material-ui/Table';
 import TableCell from 'material-ui/Table/TableCell';
-import TableRow from 'material-ui/Table/TableRow';
 import TableHead from 'material-ui/Table/TableHead';
+import TablePagination from 'material-ui/Table/TablePagination';
+import TableRow from 'material-ui/Table/TableRow';
+import { withStyles } from 'material-ui/styles';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const styles = () => ({
   root: {},
 });
 
-const List = ({ beings }) => {
-  const table = beings.map(b => (
-    <TableRow>
-      <TableCell>{b.name}</TableCell>
-      <TableCell>{b.gender}</TableCell>
-      <TableCell>{b.age}</TableCell>
-    </TableRow>
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      rowsPerPage: 25,
+    };
+    this.handleChangePage = this.handleChangePage.bind(this);
+    this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
+  }
 
-  ));
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Gender</TableCell>
-          <TableCell>Age</TableCell>
-        </TableRow>
+  handleChangePage(event, page) {
+    this.setState({ page });
+  }
+  handleChangeRowsPerPage(event, rowsPerPage) {
+    this.setState({ rowsPerPage });
+  }
 
-      </TableHead>
-      {table}
-    </Table>
+  render() {
+    const { beings } = this.props;
+    const { page, rowsPerPage } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, (beings.length - page) * rowsPerPage);
+    const table = beings.slice(page * rowsPerPage, (page * rowsPerPage) + rowsPerPage).map(b => (
+      <TableRow key={b.id}>
+        <TableCell>{b.name}</TableCell>
+        <TableCell>{b.gender}</TableCell>
+        <TableCell>{b.age}</TableCell>
+      </TableRow>
+    ));
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Age</TableCell>
+          </TableRow>
+        </TableHead>
+        {table}
+        {emptyRows > 0 && (
+          <TableRow style={{ height: 48 * emptyRows }}>
+            <TableCell colSpan={3} />
+          </TableRow>
+        )}
+        <TablePagination
+          colSpan={3}
+          count={beings.length}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={[25, 50, 75]}
+          page={page}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
+      </Table>
+    );
+  }
+}
 
-  );
-};
 
 List.propTypes = {
   beings: PropTypes.array.isRequired,
